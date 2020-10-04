3a883742a068c574ad0df64e29894279c7259f89
Alex Auritt <alexauritt@yahoo.com>
2020-10-04 14:59:31 -0700

    Display whether or not is word

--- lib/teenywords/teeny_string.ex
+++ lib/teenywords/teeny_string.ex
@@ -1,6 +1,10 @@
 defmodule Teenywords.TeenyString do
   def random(len) do
     alphabet = Enum.to_list(?A..?Z)
-    Enum.take_random(alphabet, len)
+    to_string Enum.take_random(alphabet, len)
+  end
+
+  def is_word(teeny_string) do
+    Enum.member?(Map.keys(Teenywords.Constants.two_letter_words()), String.to_atom(teeny_string))
   end
 end
--- lib/teenywords_web/controllers/page_controller.ex
+++ lib/teenywords_web/controllers/page_controller.ex
@@ -1,9 +1,10 @@
 defmodule TeenywordsWeb.PageController do
   use TeenywordsWeb, :controller
+  import Teenywords.TeenyString
 
 
   def index(conn, _params) do
     teeny_str = Teenywords.TeenyString.random(2)
-    render(conn, "index.html", teeny_str: teeny_str)
+    render(conn, "index.html", teeny_str: teeny_str, is_word: is_word(teeny_str))
   end
 end
